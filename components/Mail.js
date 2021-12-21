1d2b633e77b9f17b305cb20ecb5b699a8c9d7707
Christian <chrstnklb@gmail.com>
2021-12-21 06:15:11 +0100

    Correct mail component using parameter.

--- components/Mail.js
+++ components/Mail.js
@@ -1,18 +1,4 @@
-// import styles from './Footer.module.css'
-
-// export default function Footer() {
-//   return (
-//     <>
-//       <footer className={styles.footer}>
-//         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-//       </footer>
-//     </>
-//   )
-// }
-
 export default function Mail({ mailLink }) {
-    return <a href="{mailLink}">E-Mail senden</a>
-
-    return <h1 className="title">{title}</h1>
+    return <a href={mailLink}>E-Mail senden</a>
   }
   
