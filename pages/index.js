908b131f08d5e7d0856d6f5fce2dc05a06c74016
Christian Kliebe <chrstnklb@gmail.com>
2021-12-21 04:51:35 +0100

    Add mail as contact possibility.

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Christian Kliebe</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Christian Kliebe" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <a href="mailto:christian.kliebe@outlook.com">E-Mail senden</a>
         </p>
       </main>
 
